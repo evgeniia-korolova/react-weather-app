3a929cb79b9e29773890f3025f47269d62d2a152
evgeniia-korolova <126691505+evgeniia-korolova@users.noreply.github.com>
2023-05-21 16:16:27 +0300

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by{' '}
+        <a
+          href="https://www.shecodes.io/graduates/64804-evgeniia-korolova"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Evgeniia Korolova
+        </a>{' '}
+        and is{' '}
+        <a
+          href="https://github.com/evgeniia-korolova/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
