1fd9ce62d155f8e218d724aeef10675e217a9722
evgeniia-korolova <126691505+evgeniia-korolova@users.noreply.github.com>
2023-05-27 00:55:46 +0300

    Displayed main structure of the first day

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import WeatherInfo from './WeatherInfo';
+import WeatherForecast from './WeatherForecast';
 import './Weather.css';
 import axios from 'axios';
 
@@ -80,6 +81,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
