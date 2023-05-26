7667d57678106c4aa8e88acc42227720afc76a95
evgeniia-korolova <126691505+evgeniia-korolova@users.noreply.github.com>
2023-05-26 15:08:03 +0300

    Created component changing temperature units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import FormattedDate from './FormattedDate';
+import WeatherTemperature from './WeatherTemperature';
+
 
 export default function WeatherInfo(props) {
     return (
@@ -14,16 +16,15 @@ export default function WeatherInfo(props) {
         <div className="row mt-3">
           <div className="col-6">
             <img src={props.data.iconUrl} alt={props.data.description} />{' '}
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
+            <WeatherTemperature celsius={props.data.temperature} />
+            
+            
           </div>
           <div className="col-6">
             <ul>
               <li>Feels like : {Math.round(props.data.feelsLike)}°C</li>
               <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {Math.round(props.data.wind)} km/h</li>
+              <li>Wind: {props.data.wind} km/h</li>
             </ul>
           </div>
         </div>
